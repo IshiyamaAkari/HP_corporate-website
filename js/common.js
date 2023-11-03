54022d1370b62c43bf2f7af242d6605fb9f8b3c0
Akari.Ishiyama <arl107082@gmail.com>
2023-11-03 19:17:36 +0700

    first-commit

---
+++ js/common.js
@@ -0,0 +1,66 @@
+// glowAnimeにglowというクラス名を付ける定義
+function GlowAnimeControl() {
+  $('.glowAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("glow");
+
+    } else {
+      $(this).removeClass("glow");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  //spanタグを追加する
+    var element = $(".glowAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+        var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+window.onunload = function() {};
+function sizecheck() {
+  w = window.innerWidth ? window.innerWidth : $(window).width();
+  h = window.innerHeight ? window.innerHeight : $(window).height();
+}
+
+sizecheck();
+$(function () {
+  if (w > 769) {
+} else { 
+  $("header .drawer").click(function(){
+  $('body').toggleClass('nav-open');
+  $('header .header-wrap__nav').fadeToggle(200);
+    });
+  $("header-wrap__nav__list__item a").click(function(){
+  $('body').toggleClass('nav-open');
+  $('header .header-wrap__nav').fadeToggle(200);
+    });
+  }
+}); 
