beb46b2b773e3c47f514d112fed83571a0b0780b
Akari.Ishiyama <arl107082@gmail.com>
2023-11-05 15:32:35 +0700

    メッセージ

--- js/common.js
+++ js/common.js
@@ -66,19 +66,6 @@ $(function () {
 }); 
 
 $('.voice-wrap__con__list').slick({
-<<<<<<< HEAD
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    speed: 500,//スライドのスピード。初期値は300。
-    slidesToShow: 3,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    centerMode: true,//要素を中央ぞろえにする
-    variableWidth: true,//幅の違う画像の高さを揃えて表示
-    dots: true,//下部ドットナビゲーションの表示
-});
-=======
   autoplay: true,//自動的に動き出すか。初期値はfalse。
   infinite: true,//スライドをループさせるかどうか。初期値はtrue。
   speed: 500,//スライドのスピード。初期値は300。
@@ -90,4 +77,3 @@ $('.voice-wrap__con__list').slick({
   variableWidth: true,//幅の違う画像の高さを揃えて表示
   dots: true,//下部ドットナビゲーションの表示
 });
->>>>>>> cc921604ef4b60599297296862ef1fc6ca02d813
